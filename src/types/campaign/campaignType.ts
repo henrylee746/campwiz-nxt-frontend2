6600b3a4c5ae052d2b5f6d61bc66f0693e3f66f1
henrylee746 <henry.lee746@gmail.com>
2026-03-04 23:17:24 -0500

    Migrate CampaignEditForm.tsx component into React SPA structure

---
+++ src/types/campaign/campaignType.ts
@@ -0,0 +1,8 @@
+export const CampaignType = {
+  Categorization: "categorization",
+  COMMONS: "commons",
+  WIKIPEDIA: "wikipedia",
+  WIKIDATA: "wikidata",
+  OTHER: "other",
+} as const;
+export type CampaignType = (typeof CampaignType)[keyof typeof CampaignType];
